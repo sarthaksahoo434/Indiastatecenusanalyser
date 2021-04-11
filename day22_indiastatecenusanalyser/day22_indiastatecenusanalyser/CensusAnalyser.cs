e294ce2019480715f254e4bb6a1817a987e5b990
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-04-11 22:02:00 +0530

    [Sarthak]add:indiasenusanalyser

---
+++ day22_indiastatecenusanalyser/day22_indiastatecenusanalyser/CensusAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using day22_indiastatecenusanalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day22_indiastatecenusanalyser
+{
+   public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(string csvFilePath, Country country, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+}
